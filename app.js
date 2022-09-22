244df27e5961e55f584650ea1514907f942a3dd5
Eddie Dane <eddiedane@yahoo.com>
2022-09-22 22:38:02 +0100

    Test CI update

--- app.js
+++ app.js
@@ -6,4 +6,4 @@ app.get("/", (req, res, next) => {
   res.send("Working...");
 });
 
-app.listen(5000);
+app.listen(5050);
--- test/test.js
+++ test/test.js
@@ -1,15 +1,13 @@
 const supertest = require("supertest");
+const assert = require("assert");
 
-const server = supertest.agent("http://localhost:5000");
+const server = supertest.agent("http://localhost:5050");
 
 describe("SAMPLE unit test", () => {
   it("should return homepage", (done) => {
-    server
-      .get("/")
-      .expect("Content-type", /test/)
-      .expect(200)
-      .end((err, res) => {
-        done();
-      });
+    server.get("/").end((err, res) => {
+      assert.equal(res.status, 400);
+      done();
+    });
   });
 });
