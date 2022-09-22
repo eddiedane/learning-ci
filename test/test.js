e798c733db36723ff2902e6497ab1f2baaabdfa7
Eddie Dane <eddiedane@yahoo.com>
2022-09-22 23:14:16 +0100

    Fix mocha not exiting

--- test/test.js
+++ test/test.js
@@ -3,6 +3,6 @@ const request = require("supertest");
 
 describe("GET /", () => {
   it("responds with 200", async () => {
-    await request(app).get("/").expect(500);
+    return request(app).get("/").expect(400);
   });
 });
